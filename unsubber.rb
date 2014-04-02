dc87cb7c58d529e1157a34dc021ec6e09f312649
Matt Long <Matt@Matts-MacBook-Air.local>
2014-04-02 14:13:39 -0400

    initial commit

---
+++ unsubber.rb
@@ -0,0 +1,80 @@
+require 'net/imap'
+require 'pry'
+require 'nokogiri'
+module Unsubber
+
+  def self.login(login_info)
+    user, pass = login_info
+    email = Net::IMAP.new('imap.gmail.com', { :port => 993, :ssl => true } )
+    email.login(user, pass)
+    email.select('Inbox')
+    unsub = Unsub.new(email)
+    # unsub.get_unsub_links
+    unsub.list_sources
+    binding.pry
+    # unsub.a_tags
+  end
+
+  class Unsub
+    attr_reader :access, :record
+    def initialize(access)
+      @access = access
+      @record = {}
+    end
+
+    def list_sources
+      read_email.each do |mail|
+  
+        site = envelope(mail).first.attr["ENVELOPE"].from[0].host
+        if site
+          site_key = site.gsub(/\.\w+$/, '').gsub(/\./, '_').to_sym
+          puts "key: #{site_key} uid: #{mail}"
+          if @record[site_key]
+            @record[site_key][:count] += 1
+            @record[site_key][:uid] << mail
+          else
+            @record[site_key] = { :site => site, :count => 1, :uid => [mail] }
+          end
+        end
+      end       
+    end
+
+    def new_email
+      @access.search(["NEW"])
+    end
+
+    def read_email
+      @access.search(["NOT", "NEW"])[-100..-1]
+    end
+
+    def envelope(mail)
+      @access.fetch(mail, "ENVELOPE")
+    end
+
+    def body_tag(tag)
+      @access.fetch(tag, "BODY[TEXT]")[0].attr['BODY[TEXT]']
+    end
+
+    def get_unsub_links
+      puts "#{new_email.size} NEW"
+      puts "#{read_email.size} READ"
+      emails = read_email[-10..-1]
+      # convert to html text
+      emails.each do |email|
+        raw_html = body_tag(email)
+        if raw_html =~ /unsubscr/
+          parsed_html = parse_html(raw_html)
+          parsed_html.search('a').each do |a|
+            puts a if a.children.text =~ /unsubs/
+          end
+        end
+      end
+    end
+
+    def parse_html(raw_html)
+      Nokogiri::HTML::Document.parse(raw_html)
+    end
+  end
+end
+
+Unsubber.login ARGV
